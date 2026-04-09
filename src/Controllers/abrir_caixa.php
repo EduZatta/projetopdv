22564e6c9b05a04c5c138935532c976e7c3e2682
eduardanapoleao-cmd <eduardanapoleao@alunos.utfpr.edu.br>
2026-04-09 12:58:13 -0300

    Alteração na validação da abertura de caixa
    Foi colocado mais codigo de validação para que o valor minimo a ser aceito na abertura de caixa seja 50.

--- src/Controllers/abrir_caixa.php
+++ src/Controllers/abrir_caixa.php
@@ -2,13 +2,28 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $valor = $_POST['valor_inicial'];
 
-    // Guardamos o valor na sessão para usar depois
+    $valor = floatval($_POST['valor_inicial'] ?? 0);
+
+    // VALIDAÇÕES
+    if ($valor <= 0) {
+
+        echo "<h3>⚠️ Informe um valor válido.</h3>";
+        echo "<a href='../../views/abertura_caixa.php'>Voltar</a>";
+        exit();
+
+    } elseif ($valor < 50) {
+
+        echo "<h3>⚠️ Valor mínimo é R$ 50,00.</h3>";
+        echo "<a href='../../views/abertura_caixa.php'>Voltar</a>";
+        exit();
+
+    }
+
+    // SE PASSAR NA VALIDAÇÃO
     $_SESSION['caixa_aberto'] = true;
     $_SESSION['valor_suprimento'] = $valor;
 
-    // Depois de salvar, manda para o Dashboard
     header("Location: ../../views/dashboard.php");
     exit();
 }
